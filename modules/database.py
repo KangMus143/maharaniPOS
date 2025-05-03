64f998a3cfb8346ff81022e19d6ff73a17165ad6
KangMus143 <ihsanmusyaffa123@gmail.com>
2025-05-03 14:14:13 +0700

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def get_db_connection():
     """Mendapatkan koneksi ke database SQLite"""
-    conn = sqlite3.connect('maharanipos.db')
+    conn = sqlite3.connect('maharanipos.db')  # Nama file database Anda
     conn.row_factory = sqlite3.Row
     return conn
 
